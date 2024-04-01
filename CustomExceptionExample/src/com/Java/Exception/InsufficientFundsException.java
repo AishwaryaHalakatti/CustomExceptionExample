9d5a639771937566a91366358bf5db88a602d326
AishwaryaHalakatti <89127428+AishwaryaHalakatti@users.noreply.github.com>
2024-04-01 16:03:53 +0530

    Add files via upload

---
+++ CustomExceptionExample/src/com/Java/Exception/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+
+package com.Java.Exception;
+
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
